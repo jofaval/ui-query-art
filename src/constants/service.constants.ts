978573292d3561addbba72289258a2da1bd17309
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-12 12:49:59 +0100

    refactor(services): Implement better constants

--- src/constants/service.constants.ts
+++ src/constants/service.constants.ts
@@ -1,4 +1,10 @@
-export default {
-  BASE_URL: "https://api.artic.edu/api/v1/artworks",
-  ENDPOINTS: {},
+export const VERSION = 1;
+export const BASE_URL = `https://api.artic.edu/api/v${VERSION}`;
+export const ENDPOINTS = {
+  ARTWORKS: {
+    LIST: `${BASE_URL}/artworks`,
+  },
 };
+
+// TODO: create a nested system so that BASE_URL can be recursively nested
+// ARTWORKS: { BASE_URL: 'artworks } = `${BASE_URL}/${ARWORKS.BASE_URL}`
