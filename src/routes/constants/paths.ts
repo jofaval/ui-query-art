4f7f72d9683b2d239656f95a985fb82a95273a90
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-12 21:59:59 +0100

    refactor: Remove unused code

--- src/routes/constants/paths.ts
+++
@@ -1,9 +0,0 @@
-export const PATHS = {
-  HOME: "home",
-  ARTWORKS: {
-    INDEX: "artworks",
-    LIST: "/",
-    ENTRY: ":id",
-  },
-  FALLBACK: "*",
-};
--- src/views/errors/index.tsx
+++ src/views/errors/index.tsx
@@ -1,9 +1,7 @@
 import { createRouteConfig } from "@tanstack/react-router";
 
-import { PATHS } from "routes/constants/paths";
-
 export const errorsRoute = createRouteConfig().createRoute({
-  path: PATHS.FALLBACK,
+  path: "*",
   element: <Errors />,
 });
 
