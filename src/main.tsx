5fd11f490f337702250b3289efac3cb42d8ff794
pepe_fabra_valverde <jofaval@outlook.com>
2022-11-12 13:58:22 +0100

    refactor: Reorder imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { App } from "./App";
-
-import "./index.css";
-
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/queries/query";
 
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "@/routes/router";
+
+import { App } from "./App";
+
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 if (!rootElement?.innerHTML) {
